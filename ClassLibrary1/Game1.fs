b08836048dbe325ab7557c21460a2b012a513c62
Gerard Te <gerard.te@orbusdev.onmicrosoft.com>
2020-12-02 01:15:29 +0100

    - Added first sprite ! - Played a bit with right key to move the sprite

--- ClassLibrary1/Game1.fs
+++ ClassLibrary1/Game1.fs
@@ -2,6 +2,8 @@
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open GameState
+open Microsoft.Xna.Framework.Input
 
 type Game1 () as x =
     inherit Game()
@@ -9,6 +11,13 @@ type Game1 () as x =
     do x.Content.RootDirectory <- "Content"
     let graphics = new GraphicsDeviceManager(x)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+    let mutable bonhommeSprite = Unchecked.defaultof<Texture2D>
+    let mutable bonhommeSpriteTexture = Unchecked.defaultof<SpriteTexture>
+    let speed = 100.0;
+
+    //let mutable contentManager =  new Content.ContentManager()
+
+    let ASSET_BONHOMME_SPRITE = "bonhomme1";
 
     override x.Initialize() =
         do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
@@ -20,21 +29,34 @@ type Game1 () as x =
 
     override x.LoadContent() =
         
-         // TODO: use this.Content to load your game content here   
-        
+         // TODO: use this.Content to load your game content here        
+        bonhommeSprite <- x.Content.Load<Texture2D>(ASSET_BONHOMME_SPRITE);
+        bonhommeSpriteTexture <-  { texture = bonhommeSprite; x = 0.0;  y = 100.0; height = 100; width=100 }
         ()
  
     override x.Update (gameTime) =
 
          // TODO: Add your update logic here
-        
+        let keyboardState = Keyboard.GetState()
+
+        if(keyboardState.IsKeyDown(Keys.Right)) then
+            let posX = bonhommeSpriteTexture.x + gameTime.ElapsedGameTime.TotalSeconds
+            bonhommeSpriteTexture <-  { texture = bonhommeSprite; y = bonhommeSpriteTexture.y;  x = bonhommeSpriteTexture.x + speed*gameTime.ElapsedGameTime.TotalSeconds; height = 100; width=100 }
+        else
+            ()
         ()
  
     override x.Draw (gameTime) =
         do x.GraphicsDevice.Clear Color.Black
         
         // TODO: Add your drawing code here
+        spriteBatch.Begin();
 
+        spriteBatch.Draw(bonhommeSpriteTexture.texture, new Vector2((float32)bonhommeSpriteTexture.x,(float32)bonhommeSpriteTexture.y), Color.White)
+
+        spriteBatch.End();
+
+        base.Draw(gameTime);
         ()
 
 
---
+++ ClassLibrary1/GameState.fs
@@ -0,0 +1,24 @@
+﻿module GameState
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type SpriteTexture = {
+ x: float
+ y: float
+ height: int
+ width: int
+ texture: Texture2D 
+}
+
+type GameEntity = {
+ name: string
+ isEnabled: bool
+ position: Vector2
+ collisionBoxes: Rectangle list
+ sprite: SpriteTexture
+}
+
+type GameState = {
+   entities: GameEntity list
+}
