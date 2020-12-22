ef1f0765cc26d2058d0c711b1f618602486902c1
Gerard Te <gerard.te@orbussoftware.com>
2020-12-22 20:19:12 +0100

    More helper

--- game/Helper.fs
+++ game/Helper.fs
@@ -2,11 +2,20 @@
 module Helper
 
 /// <summary>Get first element from triplet tuple.</summary>
-let fst3 (a,b,c) = a
+let fst3 (a, b, c) = a
 
 /// <summary>Get second element from triplet tuple.</summary>
-let snd3 (a,b,c) = b
+let snd3 (a, b, c) = b
 
 /// <summary>Get third element from triplet tuple.</summary>
-let thrd3 (a,b,c) = c
+let thrd3 (a, b, c) = c
 
+
+/// <summary>Update first element from triplet tuple.</summary>
+let updateFst3 (x: 'a) (a: 'a, b, c) = (x, b, c)
+
+/// <summary>Update second element from triplet tuple.</summary>
+let updateSnd3 (x: 'a) (a, b: 'a, c) = (a, x, c)
+
+/// <summary>Update third element from triplet tuple.</summary>
+let updateThrd3 (x: 'a) (a, b, c: 'a) = (a, b, x)
