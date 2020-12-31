f5f16c3c7fdfc4341b8bc16d47733aa5d66105bd
Gerard Te <gerard.te@orbussoftware.com>
2020-12-31 12:49:36 +0100

    A first premise of level1 update position management.

--- game/Entities/Level1/Level1Update.fs
+++ game/Entities/Level1/Level1Update.fs
@@ -6,8 +6,29 @@ open Types
 open Level1Constants
 open Microsoft.Xna.Framework.Input
 
+let updateLevel1XPosition (vectorMovement: Vector2) =
+    new Vector2(vectorMovement.X * (-1f) * SPEED_MOVING_FLOOR, vectorMovement.Y)
+
+
+let updateLevel1YPosition (vectorMovement: Vector2) =
+    // maintain level background to same Y position !
+    new Vector2(vectorMovement.X, 0f)
+
 
 let updateEntity gameTime (currentGameEntity: IGameEntity) (properties: Level1Properties) =
 
-    // temp implementation
-    currentGameEntity
+    let vectorMovement =
+        KeyboardState.getMovementVectorFromKeyState (Keyboard.GetState())
+
+    let nextVectorPosition =
+        vectorMovement
+        |> updateLevel1XPosition
+        |> updateLevel1YPosition
+
+    let nextLevel1Properties = Level1Properties(properties) |> Some
+
+    let nextGameEntityProperties =
+        { currentGameEntity.Properties with
+              position = Vector2.Add(currentGameEntity.Position, nextVectorPosition) }
+
+    GameEntity.createGameEntity nextGameEntityProperties nextLevel1Properties currentGameEntity.UpdateEntity
