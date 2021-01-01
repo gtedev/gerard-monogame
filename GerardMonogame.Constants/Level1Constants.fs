45e94ea202563eb5ed3552970a0e90a0755f92c0
Gerard Te <gerard.te@orbussoftware.com>
2021-01-01 17:57:56 +0100

    Add level-1 sprite red variant.

--- GerardMonogame.Constants/Level1Constants.fs
+++ GerardMonogame.Constants/Level1Constants.fs
@@ -5,7 +5,7 @@ module Level1Constants =
 
     let EntityId = "level1"
 
-    let ASSET_LEVEL1_SPRITE = "level1"
+    let ASSET_LEVEL1_SPRITE = "level1-red"
 
     let SPEED_MOVING_FLOOR = 2f
     let LEVEL1_Y_POSITION = -100f
